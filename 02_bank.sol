cc01f1ec1d95fb815d2da2102dc1e3bef792d854
xanpena <42207094+xanpena@users.noreply.github.com>
2022-01-14 21:11:05 +0100

    Create 02_bank.sol

---
+++ 02_bank.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.17;
+
+contract Bank {
+
+    function Bank() payable public {
+
+    }
+
+    function incrementBalance(uint256 amount) payable public {
+        require(msg.value == amount);
+    }
+
+    function getBalance() public {
+        msg.sender.transfer(address(this).balance);
+    }
+}
