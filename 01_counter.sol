b5b9efbe8858eab7bcd079ae0a2c9ed0f3ebdf8e
xanpena <42207094+xanpena@users.noreply.github.com>
2022-01-14 20:53:31 +0100

    Create 01_counter.sol

---
+++ 01_counter.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Counter {
+
+    uint256 counter; 
+
+    constructor(uint256 _counter) {
+        counter = _counter;
+    }
+
+    function getCounter() public view returns(uint256) {
+        return counter;
+    }
+
+    function setCounter(uint256 _counter) public {
+        counter = _counter;
+    }
+
+    function incrementCounter() public {
+        counter += 1;
+    }
+
+    function getNumber() public pure returns(uint256) {
+        return 35;
+    }
+}
