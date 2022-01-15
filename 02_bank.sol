d5b62166d62f3037ebb38460640293f5aa9cb5aa
xanpena <42207094+xanpena@users.noreply.github.com>
2022-01-15 09:35:32 +0100

    Update 02_bank.sol
    Only owner can withdraw balance

--- 02_bank.sol
+++ 02_bank.sol
@@ -3,15 +3,18 @@ pragma solidity ^0.4.17;
 
 contract Bank {
 
-    function Bank() payable public {
+    address owner;
 
+    function Bank() payable public {
+        owner = msg.sender;
     }
 
     function incrementBalance(uint256 amount) payable public {
         require(msg.value == amount);
     }
 
-    function getBalance() public {
+    function withdrawBalance() public {
+        require(msg.sender == owner);
         msg.sender.transfer(address(this).balance);
     }
 }
